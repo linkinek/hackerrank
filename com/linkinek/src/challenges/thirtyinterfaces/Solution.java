a45d55ded42d7978a6922fac04e877bd73065e21
Ivan Timofeev <linkinek@live.ru>
2019-04-13 15:35:27 +0400

    Solved task Day 19: Interfaces

---
+++ com/linkinek/src/challenges/thirtyinterfaces/Solution.java
@@ -0,0 +1,42 @@
+package challenges.thirtyinterfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+interface AdvancedArithmetic {
+    int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                numbers.add(i);
+            }
+        }
+
+        int result = 0;
+        for (Integer number : numbers) {
+            result = result + number;
+        }
+
+        return result;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName());
+        System.out.println(sum);
+    }
+}
